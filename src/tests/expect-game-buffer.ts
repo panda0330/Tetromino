dfc9e26f06ea9ec1f63085d17cde9cdc05d063b1
panda0330 <superdev330@gmail.com>
2023-11-16 10:13:09 +0900

    game-buffer change

--- src/tests/expect-game-buffer.ts
+++ src/tests/expect-game-buffer.ts
@@ -15,39 +15,5 @@ export function bufferSize(
     width: number,
     height: number
 ) {
-    const pass =
-        width === buffer.width &&
-        height === buffer.height &&
-        buffer.rows.length === height;
-    return expectMessage(
-        this,
-        `expected buffer size to {not} be [${width}, ${height}]`,
-        pass
-    );
-}
 
-export function bufferEqual(
-    this: jest.MatcherContext,
-    buffer: GameBuffer,
-    expect: Array<string>
-) {
-    let pass = false;
-    if (
-        buffer.height === expect.length &&
-        buffer.rows.length === expect.length
-    ) {
-        pass = buffer.rows.reduce(
-            (acc: boolean, next: GameRow, indx: number) => {
-                const pattern = next.cells
-                    .map((cell) =>
-                        cell.type === undefined ? '.' : (cell.type as string)
-                    )
-                    .join('');
-                const value = next.removed ? pattern.toUpperCase() : pattern;
-                return value === expect[indx];
-            },
-            false
-        );
-    }
-    return expectMessage(this, `expected buffer to {not} equal pattern`, pass);
 }
